c23c714e70d52e4861bc4b08402cf9d94aed79bf
inconsiderate <meiskant@gmail.com>
2015-07-07 19:24:35 -0700

    configured deployment to digital ocean

--- client/index.js
+++ client/index.js
@@ -54,16 +54,3 @@ if (Meteor.isClient) {
         $('#user-panel-button').leanModal();
     });
 }
-
-var uploader = new Slingshot.Upload("myFileUploads");
-
-uploader.send(document.getElementById('input').files[0], function (error, downloadUrl) {
-    if (error) {
-        // Log service detailed response.
-        console.error('Error uploading', uploader.xhr.response);
-        alert (error);
-    }
-    else {
-        Meteor.users.update(Meteor.userId(), {$push: {"profile.files": downloadUrl}});
-    }
-});
