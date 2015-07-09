1dbf0806f18ab662956600a04471deae73ff8e09
inconsiderate <meiskant@gmail.com>
2015-07-09 03:16:26 -0700

    initial SUCCESS with uploading to AWS S3. Finally. Still many bugs.

--- server/amazon.js
+++
@@ -1,5 +0,0 @@
-S3.config = {
-    key: Meteor.settings.AMAZONKEY,
-    secret: Meteor.settings.AMAZONSECRET,
-    bucket: Meteor.settings.AMAZONBUCKET
-};
---
+++ server/amazonServer.js
@@ -0,0 +1,7 @@
+S3.config = {
+    key: Meteor.settings.AWSAccessKeyId,
+    secret: Meteor.settings.AWSSecretAccessKey,
+    bucket: Meteor.settings.S3Bucket
+};
+
+
