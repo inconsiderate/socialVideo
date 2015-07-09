92535c9a060b74e82fff7e339bb5c2db4b1b13ea
inconsiderate <meiskant@gmail.com>
2015-07-09 14:36:13 -0700

    display up uploaded videos, add uploaded videos to mongodb

--- client/lib/forms.js
+++ client/lib/forms.js
@@ -1,10 +1,9 @@
-
 Template.searchForm.events({
     "submit .search-field-form": function (event) {
         var searchFilter = event.target.searchField.value;
         Pages.set({
             filters: {
-                $text: { $search: searchFilter }
+                $text: {$search: searchFilter}
             }
         });
         event.target.searchField.value = "";
@@ -14,7 +13,7 @@ Template.searchForm.events({
 });
 
 Template.submitNewVideo.helpers({
-    "files": function(){
+    "files": function () {
         return S3.collection.find();
     }
 });
@@ -24,28 +23,31 @@ Template.submitNewVideo.events({
         var files = $("input[type='file']")[0].files;
 
         var title = event.target.video_title.value;
-        var description = event.target.video_description.value;
+        var desc = event.target.video_description.value;
         //var keywords = event.target.video_keywords.value;
 
         S3.upload({
-            files:files,
-            path:"prodvideos"
-        },function(e,r){
-            Materialize.toast(r, 4000);
-            Materialize.toast(e, 4000);
-        });
+            files: files,
+            path: "prodvideos"
+        }, function (e, r) {
+            if (e == null) {
+                Materialize.toast('Upload Successful!', 4000);
+                Meteor.call('insertVideo', title, desc, r.url);
+                //Videos.insert({
+                //    title: title,
+                //    path: r.url,
+                //    //keywords: keywords,
+                //    description: desc,
+                //    createdAt: new Date(),
+                //    owner: Meteor.userId(),
+                //    username: Meteor.user().username
+                //});
 
-        //
-        //
-        //Videos.insert({
-        //    title: title,
-        //    path: path,
-        //    keywords: keywords,
-        //    description: description,
-        //    createdAt: new Date(),
-        //    owner: Meteor.userId(),
-        //    username: Meteor.user().username
-        //});
+            } else {
+                Materialize.toast('Upload failed for some reason.', 4000);
+                Materialize.toast(e, 4000);
+            }
+        });
         //event.target.title.value = "";
         //event.target.path.value = "";
         //event.target.keywords.value = "";
--- server/collection.js
+++ server/collection.js
@@ -3,3 +3,24 @@
 Videos._ensureIndex( { "$**": "text" } );
 
 
+Meteor.methods({
+    insertVideo: function (title, desc, url) {
+
+        Videos.insert({
+            title: title,
+            path: url,
+            //keywords: keywords,
+            description: desc,
+            //createdAt: new Date(),
+            owner: Meteor.userId()
+            //username: this.user().username
+        });
+
+        //if () {
+        //    throw new Meteor.Error("pants-not-found", "Can't find my pants");
+        //}
+
+        return "some return value";
+    }
+
+});
