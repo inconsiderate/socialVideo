7f09d23f930080a77289e80d74f53075352e2a58
inconsiderate <meiskant@gmail.com>
2015-07-22 18:13:34 -0700

    removed placeholder loading gifs and added in a new one that's more suitable and less confusing

--- collections/dummyData.js
+++ collections/dummyData.js
@@ -7,7 +7,7 @@ if (Meteor.isServer && Videos.find().count() < 12) {
 
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video1.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 1',
         description: 'This is our sample video and a longer sample description because sometimes people like to type a lot of really stupid shit that no one cares about. Yup.',
         createdAt: new Date(),
@@ -17,7 +17,7 @@ if (Meteor.isServer && Videos.find().count() < 12) {
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video2.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 2',
         description: 'something more aweomse',
         createdAt: new Date(),
@@ -27,7 +27,7 @@ if (Meteor.isServer && Videos.find().count() < 12) {
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video3.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 3',
         description: 'This is video number three.',
         createdAt: new Date(),
@@ -37,7 +37,7 @@ if (Meteor.isServer && Videos.find().count() < 12) {
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video4.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 4',
         description: 'im a pinball wizard or something. boobs.',
         createdAt: new Date(),
@@ -47,47 +47,47 @@ if (Meteor.isServer && Videos.find().count() < 12) {
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video5.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 5',
-        description: 'im a pinball wizard or something. boobs.',
+        description: 'another random video.',
         createdAt: new Date(),
-        username: 'maggie',
+        username: 'alice',
         likes: Math.floor((Math.random() * 10) + 1),
         comments: 0
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video6.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 6',
-        description: 'im a pinball wizard or something. boobs.',
+        description: 'Is this a generic message? Yes. It probably is. But I only have to write it once.',
         createdAt: new Date(),
-        username: 'maggie',
+        username: 'siobhan',
         likes: Math.floor((Math.random() * 10) + 1),
         comments: 0
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video7.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 7',
-        description: 'im a pinball wizard or something. boobs.',
+        description: 'boobs. that is all.',
         createdAt: new Date(),
-        username: 'maggie',
+        username: 'other maggie',
         likes: Math.floor((Math.random() * 10) + 1),
         comments: 0
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video8.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 8',
-        description: 'im a pinball wizard or something. boobs.',
+        description: 'oh hi this is my video',
         createdAt: new Date(),
-        username: 'maggie',
+        username: 'natasha',
         likes: Math.floor((Math.random() * 10) + 1),
         comments: 0
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video9.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 9',
         description: 'im a pinball wizard or something. boobs.',
         createdAt: new Date(),
@@ -97,7 +97,7 @@ if (Meteor.isServer && Videos.find().count() < 12) {
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video10.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 10',
         description: 'im a pinball wizard or something. boobs.',
         createdAt: new Date(),
@@ -107,7 +107,7 @@ if (Meteor.isServer && Videos.find().count() < 12) {
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video11.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 11',
         description: 'im a pinball wizard or something. boobs.',
         createdAt: new Date(),
@@ -117,7 +117,7 @@ if (Meteor.isServer && Videos.find().count() < 12) {
     });
 
     Videos.insert({
-        path: 'http://s3.amazonaws.com/daddyissues/testvideos/video12.mp4',
+        path: 'http://s3.amazonaws.com/daddyissues/testvideos/df021166-e36d-424a-b4d5-4500352dd046.mp4',
         title: 'Test Video 12',
         description: 'im a pinball wizard or something. boobs.',
         createdAt: new Date(),
